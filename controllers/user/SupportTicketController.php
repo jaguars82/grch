491b8a0bbb7ce849903d4c7a32594727ee1be50d
Egor Sechin <ts-working@yandex.ru>
2022-04-20 12:29:01 +0300

    add SupportTicketForm and SupportMessageForm

--- controllers/user/SupportTicketController.php
+++ controllers/user/SupportTicketController.php
@@ -1,6 +1,11 @@
 <?php
 
 namespace app\controllers\user;
+
+use app\models\SupportTicket;
+use app\models\SupportMessage;
+use app\models\form\SupportTicketForm;
+use app\models\form\SupportMessageForm;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
 
@@ -38,17 +43,65 @@ class SupportTicketController extends \yii\web\Controller
     public function actionCreate()
     {
 
-        $model = new \app\models\SupportTickets();
+        $ticket_model = new SupportTicketForm();
+        $message_model = new SupportMessageForm();
 
-        if ($model->load(\Yii::$app->request->post())) {
-            if ($model->validate()) {
+        /*
+        if ($ticket_model->load(\Yii::$app->request->post())) {
+            if ($ticket_model->validate()) {
                 // form inputs are valid, do something here
                 return;
             }
         }
-    
+        */
+
+        /**
+         * 
+         *  THE WORK AT THIS BRANCH (SUPPORT2) WORK HAS BEEN PAUSED ON THIS SECTION
+         * 
+         */
+        /*
+        if (\Yii::$app->request->isPost && 
+            ($ticket_model->load(\Yii::$app->request->post())
+            & $ticket_model->process() & $message_model->load(\Yii::$app->request->post())  & $message_model->process())
+        ) {
+            try {
+                // Developer::create($form->attributes, $importForm->attributes);
+                $transaction = \Yii::$app->db->beginTransaction();
+
+                try {
+
+                    $ticket = (new Newbuilding())->fill($newbuildingData);
+                    $ticket->save();
+        
+                    $advantages = !empty($newbuildingData['advantages']) ? $newbuildingData['advantages'] : [];
+        
+                    foreach($advantages as $advantageId) {
+                        if($advantage = Advantage::findOne($advantageId) === null) {
+                            throw new NotFoundHttpException('Данные отсутсвуют');
+                        }
+                        $newbuilding->link('advantages', $advantage);
+                    }
+        
+                    $transaction->commit();
+                } catch(\Exception $e) {
+                    $transaction->rollBack();
+                    throw $e;
+                }
+
+                return $newbuilding;
+
+            } catch (\Exception $e) {
+                return $this->redirectBackWhenException($e);
+            }
+
+            return $this->redirectWithSuccess(['index'], 'Добавлен застройщик');
+        }
+        */
+
         return $this->render('_form', [
-            'model' => $model,
+            'ticket_model' => $ticket_model,
+            'message_model' => $message_model,
         ]);
 
         // return $this->render('create');
--- views/user/support-ticket/_form.php
+++ views/user/support-ticket/_form.php
@@ -3,28 +3,29 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
-$model->author_id = 110;
-$model->ticket_number = '78342jk'
+$ticket_model->author_id = 110;
+$ticket_model->ticket_number = '78342jk'
 
 /* @var $this yii\web\View */
-/* @var $model app\models\SupportTickets */
+/* @var $ticket_model app\models\SupportTickets */
 /* @var $form ActiveForm */
 ?>
 <div class="user-support-ticket-_form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-        <?= $form->field($model, 'author_id')->hiddenInput()->label(false) ?>
-        <?= $form->field($model, 'ticket_number')->hiddenInput()->label(false) ?>
-        <?= $form->field($model, 'is_closed') ?>
-        <?= $form->field($model, 'has_unread_messages_from_support')->hiddenInput()->label(false) ?>
-        <?= $form->field($model, 'has_unread_messages_from_author')->hiddenInput()->label(false) ?>
-        <?= $form->field($model, 'is_archived') ?>
-        <?= $form->field($model, 'last_enter_by_support')->hiddenInput()->label(false) ?>
-        <?= $form->field($model, 'last_enter_by_author')->hiddenInput()->label(false) ?>
-        <?= $form->field($model, 'created_at')->hiddenInput()->label(false) ?>
-        <?= $form->field($model, 'updated_at')->hiddenInput()->label(false) ?>
-        <?= $form->field($model, 'title') ?>
+        <?= $form->field($ticket_model, 'author_id')->hiddenInput()->label(false) ?>
+        <?= $form->field($ticket_model, 'ticket_number')->hiddenInput()->label(false) ?>
+        <?= $form->field($ticket_model, 'is_closed') ?>
+        <?= $form->field($ticket_model, 'has_unread_messages_from_support')->hiddenInput()->label(false) ?>
+        <?= $form->field($ticket_model, 'has_unread_messages_from_author')->hiddenInput()->label(false) ?>
+        <?= $form->field($ticket_model, 'is_archived') ?>
+        <?= $form->field($ticket_model, 'last_enter_by_support')->hiddenInput()->label(false) ?>
+        <?= $form->field($ticket_model, 'last_enter_by_author')->hiddenInput()->label(false) ?>
+        <?= $form->field($ticket_model, 'created_at')->hiddenInput()->label(false) ?>
+        <?= $form->field($ticket_model, 'updated_at')->hiddenInput()->label(false) ?>
+        <?= $form->field($ticket_model, 'title') ?>
+        <?= $form->field($message_model, 'text') ?>
     
         <div class="form-group">
             <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
