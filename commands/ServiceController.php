95d3e788cb76361161fe97fc049c67cc621c7f80
Egor Sechin <ts-working@yandex.ru>
2022-05-04 16:29:37 +0300

    add ServiceController prototype to app\components

---
+++ commands/ServiceController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+
+class ServiceController extends Controller
+{
+    public function actionHideZeroPositions() {
+        echo 'Hi! I am working!';
+    }
+}
