7647b86cc8024c1cf17b682c94957b4af28f610b
Egor Sechin <jaguars82@yandex.ru>
2022-06-10 18:14:20 +0300

    layuot switcher pictogramm buttons

--- views/widgets/layouts.php
+++ views/widgets/layouts.php
@@ -8,49 +8,47 @@ LayoutsAsset::register($this);
 ?>
 
 <div style="display: flex;">
-<div id="placeholder">
-    <div id="area0" class="layout" style="width: 100%; height: 100%; max-height: 100%;">
-        <div style="position: relative;">
-            <?= $this->render('/widgets/compass-rose', [
-                'id' => 'compass-rose-flat',
-                'azimuth' => $flat->entrance->azimuth
-            ]) ?>
-        </div>
-        <?= Html::img(["/uploads/{$flat->layout}"], ['id' => 'flat-layout', 'style' => 'display: block; max-height: 100%; margin: 0 auto;']) ?>
-    </div>
-    <div id="area1" class="layout" style="display: none; width: 100%; height: 100%; max-height: 100%;">
-        <div style="position: relative;">
+    <div id="placeholder">
+        <div id="area0" class="layout" style="width: 100%; height: 85%; max-height: 85%;">
+            <div style="position: relative;">
                 <?= $this->render('/widgets/compass-rose', [
-                'id' => 'compass-rose-entrance',
-                'azimuth' => $flat->entrance->azimuth
-            ]) ?>
+                    'id' => 'compass-rose-flat',
+                    'azimuth' => $flat->entrance->azimuth
+                ]) ?>
+            </div>
+            <?= Html::img(["/uploads/{$flat->layout}"], ['id' => 'flat-layout', 'style' => 'display: block; max-height: 100%; margin: 0 auto;']) ?>
         </div>
-        <div id="entrance-layout" style="width: 400px; max-width: 400px; display: block; margin: 50px 50px;">
-            <?= $floorLayoutImage ?>
-        </div>
-    </div>
-    <div id="area2" class="layout" style="display: none; width: 100%; height: 100%; max-height: 100%;">
-        <!--<img src="https://dbldom.ru/wp-content/uploads/2021/10/12b2.jpg" />-->
-        <img src="/uploads/<?= $flat->newbuildingComplex->master_plan ?>" style="display: block; max-height: 100%; margin: 0 auto;" />
-    </div>
-</div>
-<ul id="thumbs">
-    <li>
-        <a id="thumb1" data-index="0" href="#">
-        <?= Html::img(["/uploads/{$flat->layout}"], ['id' => 'flat-layout-thumb', 'style' => 'display: block; max-height: 100%; margin: 0 auto;']) ?>
-        </a>
-    </li>
-    <li>
-        <a id="thumb2" data-index="1" href="#">
-            <div>
+        <div id="area1" class="layout" style="display: none; width: 100%; height: 100%; max-height: 100%;">
+            <div style="position: relative;">
+                    <?= $this->render('/widgets/compass-rose', [
+                    'id' => 'compass-rose-entrance',
+                    'azimuth' => $flat->entrance->azimuth
+                ]) ?>
+            </div>
+            <div id="entrance-layout" style="width: 450px; max-width: 450px; display: block; margin: 100px 100px;">
                 <?= $floorLayoutImage ?>
             </div>
-        </a>
-    </li>
-    <li>
-        <a id="thumb3" data-index="2" href="#">
+        </div>
+        <div id="area2" class="layout" style="display: none; width: 100%; height: 100%; max-height: 100%;">
+            <!--<img src="https://dbldom.ru/wp-content/uploads/2021/10/12b2.jpg" />-->
             <img src="/uploads/<?= $flat->newbuildingComplex->master_plan ?>" style="display: block; max-height: 100%; margin: 0 auto;" />
-        </a>
-    </li>
-</ul>
+        </div>
+    </div>
+    <ul id="thumbs">
+        <li>
+            <a id="thumb1" data-index="0" href="#" title="Квартира">
+                <img src="/img/icons/layout-flat.svg" />
+            </a>
+        </li>
+        <li>
+            <a id="thumb2" data-index="1" href="#" title="Этаж">
+            <img src="/img/icons/layout-floor.svg" />
+            </a>
+        </li>
+        <li>
+            <a id="thumb3" data-index="2" href="#"  title="Генплан">
+                <img src="/img/icons/layout-genplan.svg" />
+            </a>
+        </li>
+    </ul>
 </div>
--- web/js/widgets/layouts.js
+++ web/js/widgets/layouts.js
@@ -12,11 +12,26 @@ $(function() {
         /*if (currentEffect) {
             currentEffect.stop();
         }*/
-        
+
         $(".layout").hide();
         $("#area" + target).show();
         // currentEffect = kendo.fx("#area" + target).replace("#area" + index, "swap");
         // currentEffect.run();
         index = target;
     });
+});
+
+/** tooltips */
+$(document).ready(function() {
+    var tooltip = $("#thumbs").kendoTooltip({
+        filter: "a",
+        width: 120,
+        position: "left",
+        animation: {
+            open: {
+                effects: "zoom",
+                duration: 150
+            }
+        }
+    }).data("kendoTooltip");
 });
