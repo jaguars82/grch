962e162814bc5f58a2e11aa1868634f81fd6e1b1
Egor Sechin <ts-working@yandex.ru>
2022-06-20 17:04:14 +0300

    begin to modify getCashPriceWithDiscount method in Flat model

--- models/Flat.php
+++ models/Flat.php
@@ -286,7 +286,26 @@ class Flat extends ActiveRecord
      */
     public function getCashPriceWithDiscount()
     {
-        return $this->price_cash * (1 - $this->discount);
+        /*var_dump($this->discount_type);
+        var_dump($this->price_cash); 
+        var_dump($this->discount_amount);*/
+
+        switch ($this->discount_type) {
+            case 0:
+                //$priceWithDiscount = $this->price_cash * (1 - $this->discount);
+                return $this->price_cash * (1 - $this->discount);
+                break;
+            case 1:
+                break;
+                //$priceWithDiscount = $this->price_cash - $this->discount_amount;
+                return (int) $this->price_cash - (int) $this->discount_amount;
+            case 2:
+                //$priceWithDiscount = $this->discount_price;
+                return $this->discount_price;
+                break;
+        }
+        // return $this->price_cash * (1 - $this->discount);
+        // return $priceWithDiscount;
     }
 
     /**
