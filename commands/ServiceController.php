85caade06c8f7ec6acbb873f7bd1dd770e40a85b
Egor Sechin <jaguars82@yandex.ru>
2022-07-04 07:19:52 +0300

    add 'feed-inquiry' command to commands/ServiceController

--- commands/ServiceController.php
+++ commands/ServiceController.php
@@ -3,8 +3,9 @@
 namespace app\commands;
 
 use yii\console\Controller;
-use app\models\Newbuilding;
+use app\models\service\Developer;
 use app\models\NewbuildingComplex;
+use app\models\Newbuilding;
 use app\models\News;
 
 class ServiceController extends Controller
@@ -39,6 +40,7 @@ class ServiceController extends Controller
         }        
     }
 
+    
     public function actionDisableExpiredActions() {
 
         $actions = (new News())->actions;
@@ -62,4 +64,70 @@ class ServiceController extends Controller
             }
         }
     }
+
+
+	public function actionFeedInquiry() {
+
+        $id = 13;
+
+        if (($developer = Developer::findOne($id)) === null) {
+            echo "Застройщик с ID $id не найден\n";
+            return self::RETURN_ERROR_NOT_FOUND;
+        }
+        
+        if (is_null($import = $developer->import)) {
+            echo "Для застройщика не настроен импорт\n";
+            return self::RETURN_ERROR_NO_IMPORT;
+        }
+
+        $algorithm = $import->algorithmAsObject;
+
+        $data = $algorithm->getAndParse($import->endpoint);
+		
+		$dataTree = array();
+		
+		foreach ($data['newbuildingComplexes'] as $nbc_key => $newbuildingComplex) {
+			$newbuildingComplexFeedName = $newbuildingComplex['name'];
+			
+			foreach ($data['newbuildings'] as $nb_key => $newbuilding) {
+				if ($newbuilding['objectId'] === $nbc_key) {
+					$newbuildingFeedName = $newbuilding['name'];
+					
+					$dataTree[$newbuildingComplexFeedName][$newbuildingFeedName] = [
+						//$newbuildingFeedName => [
+							'flats' => array(),
+							'flats_in_feed_amount' => 0,
+							'flats_in_feed_numbers' => array()
+						//]
+					];
+					
+					$newbuildingFlatCounter = 0;
+					
+					foreach ($data['flats'] as $flat) {
+						if ($flat['houseId'] === $nb_key) {
+							$newbuildingFlatCounter += 1;
+							//$dataTree[$newbuildingComplexFeedName][$newbuildingFeedName]['flats'][] = $flat;
+							$dataTree[$newbuildingComplexFeedName][$newbuildingFeedName]['flats_in_feed_amount'] = $newbuildingFlatCounter;
+							$dataTree[$newbuildingComplexFeedName][$newbuildingFeedName]['flats_in_feed_numbers'][] = $flat['number'];
+						}
+					}
+				}
+			}
+		}
+		
+		// list positions from feeds
+        // var_dump($dataTree);
+		foreach ($dataTree as $newbuildingComplexName => $buildings) {
+			echo '----- '.$newbuildingComplexName.' -----';
+			// var_dump($buildings);
+			echo PHP_EOL;
+			foreach ($buildings as $buildingName => $buildingData) {
+				echo $buildingName;
+				echo PHP_EOL;
+				echo 'передаётся '.$buildingData['flats_in_feed_amount'].' квартир';
+				echo PHP_EOL;
+			}
+		}
+    }
+    
 }
