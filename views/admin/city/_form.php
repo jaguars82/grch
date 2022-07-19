5e1df6f7fbee8be9515cb7b9642478535d9c9e8e
Egor Sechin <ts-working@yandex.ru>
2022-07-19 10:10:56 +0300

    add 'is_region_center' & 'is_district_center' filds in city form

--- views/admin/city/_form.php
+++ views/admin/city/_form.php
@@ -15,31 +15,67 @@ CityFormAsset::register($this);
 <div class="city-form">
 
     <?php $form = ActiveForm::begin(); ?>
-    
-    <?= $form->field($model, 'region_id', [
-        'options' => [
-            'class' => 'form-group inline-select'
-        ]
-    ])->dropDownList($regions, [
-        'prompt' => '',
-        'data-placeholder' => 'Регион'
-    ])->label(false) ?>
-    
-    <?= $form->field($model, 'region_district_id', [
-        'options' => [
-            //'id' => 'region-district-select',
-            'class' => 'form-group inline-select'
-        ]
-    ])->dropDownList($region_districts, [
-        'prompt' => '',
-        'data-placeholder' => 'Район'
-    ])->label(false) ?>
 
-    <?= $form->field($model, 'name')->widget(InputAddress::className(), ['form' => $form, 'attribute' => 'name']) ?>
+    <div class="row">
 
-    <div class="form-group">
-        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
-        <?= Html::a('Отмена', $backUrl, ['class' => 'btn btn-danger']) ?>
+        <div class="col-md-6">
+        
+            <?= $form->field($model, 'region_id', [
+                'options' => [
+                    'class' => 'form-group inline-select'
+                ]
+            ])->dropDownList($regions, [
+                'prompt' => '',
+                'data-placeholder' => 'Регион'
+            ])->label(false) ?>
+
+        </div>
+
+        <div class="col-md-6">
+        
+            <?= $form->field($model, 'region_district_id', [
+                'options' => [
+                    //'id' => 'region-district-select',
+                    'class' => 'form-group inline-select'
+                ]
+            ])->dropDownList($region_districts, [
+                'prompt' => '',
+                'data-placeholder' => 'Район'
+            ])->label(false) ?>
+
+        </div>
+
+        <div class="col-md-12">
+
+            <?= $form->field($model, 'name')->widget(InputAddress::className(), ['form' => $form, 'attribute' => 'name']) ?>
+
+        </div>
+
+        <div class="col-md-6">
+        
+            <?= $form->field($model, 'is_region_center',[
+                        'template' => "<label>{input}<span>Региональный центр</span></label>{error}",
+                    ])->checkbox([], false)->label(false) ?>
+
+        </div>
+
+        <div class="col-md-6">
+
+            <?= $form->field($model, 'is_district_center',[
+                        'template' => "<label>{input}<span>Районный центр</span></label>{error}",
+                    ])->checkbox([], false)->label(false) ?>
+
+        </div>
+
+        <div class="col-md-12">
+
+            <div class="form-group">
+                <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
+                <?= Html::a('Отмена', $backUrl, ['class' => 'btn btn-danger']) ?>
+            </div>
+
+        </div>
+
     </div>
 
     <?php ActiveForm::end(); ?>
