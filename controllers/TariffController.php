32132d1b2d878a1f9d3a01524c96e0a60758eafb
Egor Sechin <jaguars82@yandex.ru>
2023-01-17 09:48:44 +0300

    add tariff controller & view

---
+++ controllers/TariffController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace app\controllers;
+
+use app\components\traits\CustomRedirects;
+use app\models\Developer;
+use app\models\Tariff;
+use yii\data\ActiveDataProvider;
+use tebe\inertia\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\AccessControl;
+use yii\helpers\ArrayHelper;
+
+/**
+ * TariffController implements the CRUD actions for Faq model.
+ */
+class TariffController extends Controller
+{
+    use CustomRedirects;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['index'],
+                        'roles' => ['@'],
+                    ],
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Faq models.
+     * @return mixed
+     */
+    public function actionIndex($id = null)
+    {
+        if (empty($id)) {
+            $model = Tariff::find()->orderBy('id DESC')->one();
+        } else {
+            $model = Tariff::findOne($id);
+        }
+
+        $model = ArrayHelper::toArray($model);
+
+        $developers = (new Developer())->find()->all();
+        $developersArr = Array();
+
+        foreach ($developers as $developer) {
+            $developerRow = ArrayHelper::toArray($developer);
+            $developerRow['complexes'] = [];
+            foreach ($developer->newbuildingComplexes as $complex) {
+                if (array_key_exists($complex->id, $model['tariff_table'])) {
+                    $complexRow = ArrayHelper::toArray($complex);
+                    $complexRow['tariffs'] = $model['tariff_table'][$complex->id]['tariffs'];
+                    $complexRow['termsOfPayment'] = $model['tariff_table'][$complex->id]['termsOfPayment'];
+                    $developerRow['complexes'][] = $complexRow;
+                }
+            }
+            array_push($developersArr, $developerRow);
+        }
+
+        // echo '<pre>'; var_dump($developersArr); echo '</pre>'; die;
+
+        return $this->inertia('Tariff/Index', [
+            'model' => $model,
+            'developers' => $developersArr
+        ]);
+    }
+
+}
---
+++ resources/js/Pages/Tariff/Index.vue
@@ -0,0 +1,74 @@
+<template>
+  <MainLayout>
+    <template v-slot:main>
+      <q-card class="q-my-md shadow-7">
+        <q-card-section>
+          <h3 class="text-center">Таблица тарифов</h3>
+        </q-card-section>
+        <q-card-section>
+          <div v-for="developer of developers" :key="developer.id">
+            <template v-if="developer.complexes.length">
+              <p class="text-h4">{{ developer.name }}</p>
+              <table class="tarifftable full-width">
+                <tr>
+                  <th class="text-center">Жилой комплекс</th>
+                  <th class="text-center">Размер вознаграждения</th>
+                  <th class="text-center">Сроки выплаты вознаграждения</th>
+                </tr>
+                <tr v-for="complex of developer.complexes" :key="complex.id">
+                  <td>{{ complex.name }}</td>
+                  <td>
+                    <div v-for="(tariff, i) of complex.tariffs" :key="i">
+                      <span v-if="tariff.tariffType === 'percent'">{{ tariff.amountPercent }}%</span>
+                      <span v-else-if="tariff.tariffType === 'currency'">{{ tariff.amountCurrency }} ₽</span>
+                      <span v-else-if="tariff.tariffType === 'custom'">{{ tariff.amountCustom }}</span>
+                      <span v-if="tariff.annotation"> - {{ tariff.annotation }}</span>
+                    </div>
+                  </td>
+                  <td>{{ complex.termsOfPayment }}</td>
+                </tr>
+              </table>
+            </template>
+          </div>
+        </q-card-section>
+      </q-card>
+    </template>
+  </MainLayout>
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+import { Inertia } from '@inertiajs/inertia'
+import MainLayout from '@/Layouts/MainLayout.vue'
+import Loading from "@/Components/Elements/Loading.vue"
+
+export default {
+  props: {
+    model: {
+      type: Array,
+      derfault: []
+    },
+    developers: {
+      type: Array,
+      default: []
+    }
+  },
+  components: {
+    MainLayout, Loading
+  },
+  setup() {
+    
+  },
+}
+</script>
+
+<style scoped>
+.tarifftable {
+  border-collapse: collapse;
+}
+
+table td, table th {
+  border: 1px solid #888;
+  padding: 5px 10px;
+}
+</style>
