48ac88f7c3a3e345d918967d752e16649c6c0280
Egor Sechin <jaguars82@yandex.ru>
2024-01-10 15:00:49 +0300

    add photo preview to user profile form

--- resources/js/Components/Person/UserProfileForm.vue
+++ resources/js/Components/Person/UserProfileForm.vue
@@ -10,47 +10,90 @@
       <div class="col-12 col-md-4 q-py-xs">
         <q-input outlined v-model="formfields.last_name" label="Фамилия" />
       </div>
-      <div class="col-12 col-sm-6 q-py-xs">
-        <q-input outlined v-model="formfields.email" label="Электронная почта" />
-      </div>
-      <div class="col-12 col-sm-6 q-py-xs">
-        <q-input outlined v-model="formfields.phone" label="Телефон" />
-      </div>
-      <div class="col-12 col-sm-6 q-py-xs">
-        <q-input outlined v-model="formfields.telegram_id" label="Telegram ID" />
-      </div>
-      <div class="col-12 col-sm-6 q-py-xs">
-        <q-input outlined v-model="formfields.telegram_chat_id" label="Telegram Chat ID" />
-      </div>
-      <div class="col-12 q-pr-md q-py-xs">
-        <q-file
-          outlined
-          v-model="image"
-          label="Перетащите или загрузите фото"
-          use-chips
-          accept=".jpg, image/*"
-        >
-          <template v-slot:prepend>
-            <q-icon name="attach_file" />
-          </template>
-          <template v-slot:file="{ index, file }">
-            <q-chip
-              :removable="true"
-              @remove="image = ''"
-            >
-              <q-avatar>
-                <q-icon name="photo" />
-              </q-avatar>
-              <div class="ellipsis relative-position">
-                {{ file.name }}
-              </div>
-            </q-chip>
-          </template>
-        </q-file>
-      </div>
 
+      <div class="row q-col-gutter-none">
+
+        <div class="col-12 col-sm-7">
+          <div class="row q-col-gutter-none">
+
+            <div class="col-12 col-sm-6 q-py-xs" :class="{ 'q-pr-none': $q.screen.xs }">
+              <q-input outlined v-model="formfields.email" label="Электронная почта" />
+            </div>
+            <div class="col-12 col-sm-6 q-py-xs q-pr-none">
+              <q-input outlined v-model="formfields.phone" label="Телефон" />
+            </div>
+            <div class="col-12 col-sm-6 q-py-xs" :class="{ 'q-pr-none': $q.screen.xs }">
+              <q-input outlined v-model="formfields.telegram_id" label="Telegram ID" />
+            </div>
+            <div class="col-12 col-sm-6 q-py-xs q-pr-none">
+              <q-input outlined v-model="formfields.telegram_chat_id" label="Telegram Chat ID" />
+            </div>
+            <div class="col-12 q-py-xs">
+              <q-file
+                outlined
+                v-model="image"
+                label="Перетащите или загрузите фото"
+                use-chips
+                accept=".jpg, image/*"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="attach_file" />
+                </template>
+                <template v-slot:file="{ index, file }">
+                  <q-chip
+                    :removable="true"
+                    @remove="image = ''"
+                  >
+                    <q-avatar>
+                      <q-icon name="photo" />
+                    </q-avatar>
+                    <div class="ellipsis relative-position">
+                      {{ file.name }}
+                    </div>
+                  </q-chip>
+                </template>
+              </q-file>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="col-12 q-py-xs col-sm-5">
+          <q-img v-if="photoPreview" class="rounded-borders photo" :src="photoPreview" />
+          <q-img v-else :fit="formfields.is_photo_reset ? 'scale-down' : ''" class="rounded-borders photo" :src="user.photo && !formfields.is_photo_reset ? `/uploads/${user.photo}` : '/img/blank-person.svg'">
+            <q-icon
+              v-if="!formfields.is_photo_reset"
+              class="absolute cursor-pointer all-pointer-events"
+              size="28px"
+              name="close"
+              color="white"
+              style="top: 4px; right: 4px"
+              @click="onResetPhoto"
+            >
+              <q-tooltip>
+                Удалить текущую фотографию
+              </q-tooltip>
+            </q-icon>
+            <q-icon
+              v-if="formfields.is_photo_reset"
+              class="absolute cursor-pointer all-pointer-events"
+              size="28px"
+              name="refresh"
+              color="grey-7"
+              style="top: 4px; right: 4px"
+              @click="onRestorePhoto"
+            >
+              <q-tooltip>
+                Восстановить фото
+              </q-tooltip>
+            </q-icon>
+          </q-img>
+        </div>
+
+      </div>
     </div>
-    <div class="row justify-end q-px-md q-pt-md">
+
+    <div class="row justify-end q-px-md q-pt-lg">
       <q-btn type="submit" padding="xs md" unelevated rounded color="primary" icon="done" label="Сохранить" :disable="!canSubmitForm" />
       <q-btn class="q-ml-xs" padding="xs md" unelevated rounded icon="close" label="Отмена" @click="onCancel" />
     </div>
@@ -58,7 +101,7 @@
 </template>
 
 <script>
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
 
 export default {
@@ -79,9 +122,33 @@ export default {
       telegram_id: props.user.telegram_id ? props.user.telegram_id : '',
       telegram_chat_id: props.user.telegram_chat_id ? props.user.telegram_chat_id : '',
       photo: '',
+      is_photo_reset: false
     })
 
     const image = ref('')
+
+    const photoPreview = ref('')
+
+    watch(image, () => {
+      if (image.value) {
+        let reader = new FileReader()
+        reader.readAsDataURL(image.value)
+        reader.onload = function(e) {
+          let data = reader.result
+          photoPreview.value = data
+        }
+      } else {
+        photoPreview.value = ''
+      }
+    })
+
+    const onResetPhoto = () => {
+      formfields.value.is_photo_reset = true
+    }
+
+    const onRestorePhoto = () => {
+      formfields.value.is_photo_reset = false
+    }
 
     const canSubmitForm = computed(() => {
       if (formfields.value.first_name === '') return false
@@ -99,7 +166,13 @@ export default {
       Inertia.get(props.redirectUrl)
     }
 
-    return { formfields, image, canSubmitForm, onSubmitForm, onCancel }
+    return { formfields, image, photoPreview, onResetPhoto, onRestorePhoto, canSubmitForm, onSubmitForm, onCancel }
   }
 }
 </script>
+
+<style scoped>
+.photo {
+  max-height: 200px;
+}
+</style>
