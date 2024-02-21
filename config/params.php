e5b8bd08243762ef1d050043fa0ec4b7c55e3799
Egor Sechin <jaguars82@yandex.ru>
2024-02-21 11:51:02 +0300

    change email transport params

--- config/params.php
+++ config/params.php
@@ -1,8 +1,10 @@
 <?php
 
 return array_merge([
-    'adminEmail' => 'grch.dev@yandex.ru',
-    'senderEmail' => 'grch.dev@yandex.ru',
+    // 'adminEmail' => 'grch.dev@yandex.ru',
+    // 'senderEmail' => 'grch.dev@yandex.ru',
+    'adminEmail' => 'loader@grvrn.ru',
+    'senderEmail' => 'loader@grvrn.ru',
     'senderName' => 'ГРЧ',
     'webroot_alias' => 'web',
 ], require __DIR__ . '/private_params.php');
