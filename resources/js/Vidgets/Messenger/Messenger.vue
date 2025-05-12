87bf1d3e691b81afad12e2f8af50006b4c81f000
Egor Sechin <jaguars82@yandex.ru>
2025-05-12 14:25:05 +0300

    make Vidgets/Messenger component prototype, add it to Secondary/View page

---
+++ resources/js/Vidgets/Messenger/Messenger.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>Сообщения</p>
+  <div class="chat"></div>
+  <q-input v-model="message" />
+  <q-btn label="отправить" @click="sendMessage" />
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const message = ref(null)
+    const sendMessage = () => {
+      console.log(message.value)
+      // Do things
+    }
+    return { message, sendMessage }
+  }
+}
+</script>
